964fc507fc924aa4ec934eddbcc58e6222febdce
lupeaugier <graugier@gmail.com>
2020-09-17 11:41:40 -0400

    Ejercicio clase HTML

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+
+const app = express()
+
+const path = require('path')
+
+
+
+app.get('/', function (req, res){
+    let file = path.resolve('index.html')
+    res.sendFile(file)
+});
+
+
+app.get('*', function (req, res){
+    if (req.url.endsWith('.css')){
+        let file = path.resolve('syles' + req.url)
+        return res.sendFile(file)
+    }
+
+
+let images = ['.jpg', 'jpeg', '.png', '.bmp', 'svg'];
+let ext = req.url.split('.')[1];
+
+if (images.includes(ext)){
+    let file = path.resolve('images' + req.url);
+    return res.sendFile(file);
+}; 
+    res.send("Not Found");
+
+});
+
+app.listen(3000, () => {
+    console.log("servidor corriendo");
+});
