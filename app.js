769bac2dfc2ff85e993ce07008a4a2a930c99009
lupeaugier <graugier@gmail.com>
2020-09-24 16:21:39 -0400

    Entrega de registro y login

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.get('/', function (req, res){
     res.sendFile(file)
 });
 
+app.get('/register', function (req, res){
+    let file = path.resolve('register.html')
+    res.sendFile(file)
+});
+
+
+app.get('/login', function (req, res){
+    let file = path.resolve('login.html')
+    res.sendFile(file)
+});
 
 app.get('*', function (req, res){
     if (req.url.endsWith('.css')){
